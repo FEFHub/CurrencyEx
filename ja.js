6d19fb9c3de75b24bb3d76114ab51759d7d58421
FEFHub <100193997+FEFHub@users.noreply.github.com>
2022-03-15 23:19:20 +0530

    Changed

--- ja.js
+++ ja.js
@@ -1,25 +1,21 @@
-$.getJSON("http://api.exchangeratesapi.io/v1/latest?access_key=760edb47c69471caf3e1acf30445d787",
-function(data){
-  console.log(data);
-
-  var dis= 1/data.rates.INR;
-  $(".dis").append(dis+" EUR");
+fetch("http://api.exchangeratesapi.io/v1/latest?access_key=760edb47c69471caf3e1acf30445d787")
+.then(response=>response.json())
+.then(data=>getExchangeRate(data));
+function getExchangeRate(data){
+  $(".dis").append((1/data.rates.INR)+" EUR");
   getButton = document.querySelector("form button");
 getButton.addEventListener("click", e =>{
     e.preventDefault();
-    getExchangeRate();
-});
-
-  function getExchangeRate(){
+    var dis= 1/data.rates.INR;
     const amount = document.querySelector("form input");
     const eTxt = document.querySelector(".display");
     let amountVal = amount.value;
-    let d= amountVal * dis;
+    let d= amountVal *dis;
     eTxt.innerText = "";
     $(".display").append(d);
     if(amountVal == "" || amountVal == "0"){
         document.querySelector(".inp");
     }
-    console.clear(d);
-  }
-});
+  });
+  console.clear();
+};
