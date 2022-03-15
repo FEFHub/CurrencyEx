8b2d8fbed4302602deeaed9144086b7bb856c80a
FEFHub <100193997+FEFHub@users.noreply.github.com>
2022-03-16 00:20:43 +0530

    Beautify

--- ja.js
+++ ja.js
@@ -1,26 +1,27 @@
 fetch("https://currency-exchange.p.rapidapi.com/exchange?from=INR&to=EUR&q=1.0", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
-		"x-rapidapi-key": "353fc2605emshf52ca478edc11a4p160f85jsn20a73a271e73"
-	}
-})
-.then(response=>response.json())
-.then(data=>getExchangeRate(data));
-function getExchangeRate(data){
-  $(".dis").append(data+" EUR");
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+      "x-rapidapi-key": "353fc2605emshf52ca478edc11a4p160f85jsn20a73a271e73"
+    }
+  })
+  .then(response => response.json())
+  .then(data => getExchangeRate(data));
+
+function getExchangeRate(data) {
+  $(".dis").append(data + " EUR");
   getButton = document.querySelector("form button");
-getButton.addEventListener("click", e =>{
+  getButton.addEventListener("click", e => {
     e.preventDefault();
-    var dis= data;
+    var dis = data;
     const amount = document.querySelector("form input");
     const eTxt = document.querySelector(".display");
     let amountVal = amount.value;
-    let d= amountVal * dis;
+    let d = amountVal * dis;
     eTxt.innerText = "";
     $(".display").append(d);
-    if(amountVal == "" || amountVal == "0"){
-        document.querySelector(".inp");
+    if (amountVal == "" || amountVal == "0") {
+      document.querySelector(".inp");
     }
   });
   console.log(data);
