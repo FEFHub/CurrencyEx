db23a84788c42ece2064310a85dfcbcd2896c95d
FEFHub <100193997+FEFHub@users.noreply.github.com>
2022-03-15 23:49:56 +0530

    Using HTTPS

--- ja.js
+++ ja.js
@@ -1,21 +1,27 @@
-fetch("http://api.exchangeratesapi.io/v1/latest?access_key=760edb47c69471caf3e1acf30445d787")
+fetch("https://currency-exchange.p.rapidapi.com/exchange?from=INR&to=EUR&q=1.0", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+		"x-rapidapi-key": "353fc2605emshf52ca478edc11a4p160f85jsn20a73a271e73"
+	}
+})
 .then(response=>response.json())
 .then(data=>getExchangeRate(data));
 function getExchangeRate(data){
-  $(".dis").append((1/data.rates.INR)+" EUR");
+  $(".dis").append(data+" EUR");
   getButton = document.querySelector("form button");
 getButton.addEventListener("click", e =>{
     e.preventDefault();
-    var dis= 1/data.rates.INR;
+    var dis= data;
     const amount = document.querySelector("form input");
     const eTxt = document.querySelector(".display");
     let amountVal = amount.value;
-    let d= amountVal *dis;
+    let d= amountVal * dis;
     eTxt.innerText = "";
     $(".display").append(d);
     if(amountVal == "" || amountVal == "0"){
         document.querySelector(".inp");
     }
   });
-  console.clear();
+  console.log(data);
 };
